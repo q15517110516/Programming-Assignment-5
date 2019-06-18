9fd2f9b5d3ae1df3a98f5cf838d92dd8f9e52704
Mingrui Liu <875139377@qq.com>
2019-06-18 16:54:42 -0400

    Delete ud_thread.h

--- ud_thread.h
+++
@@ -1,8 +0,0 @@
-///* 
-// * thread library function prototypes
-// */
-//void t_create(void(*function)(int), int thread_id, int priority);
-//void t_yield(void);
-//void t_init(void);
-//void t_terminate();
-//void t_shutdown();
